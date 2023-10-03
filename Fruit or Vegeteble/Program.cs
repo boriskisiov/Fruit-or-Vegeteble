77887a68b305f371937419454e46ae647b35f014
boriskisiov <boriskisiov@gmail.com>
2023-10-03 15:43:49 +0300

    Add project files.

---
+++ Fruit or Vegeteble/Program.cs
@@ -0,0 +1,12 @@
+﻿string pro = Console.ReadLine();
+
+if (pro == "banana" || pro == "apple" || pro == "kiwi" || pro == "cherry" || pro == "lemon")
+{
+    Console.WriteLine("Fruit");
+}
+else if (pro == "cucumber" || pro == "pepper" || pro == "carrot")
+{
+    Console.WriteLine("Vegeteble");
+}
+else
+    Console.WriteLine("onknown");
